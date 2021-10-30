1f5c74cecfdc529602619f5f140f37feed139831
Daniil-Vlasenko <74912458+Daniil-Vlasenko@users.noreply.github.com>
2021-10-30 22:12:38 +0300

    Внес правки задания 1

--- task1/task1.R
+++ task1/task1.R
@@ -4,10 +4,10 @@ library("lubridate")
 # 1.
 # Create a dateframe of self-employed.
 df <- read.table("study_fall2021_intror_creditcard.txt", header=TRUE, sep=";")
-df <- df[ df[, "selfemp"] == "yes", ]
+df <- subset(df, selfemp == "yes",)
 # Convert amount of months applicant living at his/her current address to days.
 today <- as.Date("2000-01-01")
-df[, "months"] <- sapply(df["months"], FUN = function(x) as.numeric(today - (today %m-% months(x))))
+df[, "months"] <- lapply(df["months"], function(x) as.numeric(today - (today %m-% months(x))))
 names(df)[names(df) == "months"] <- "days"
 # Add column with income per family member.
 # Due to the lack of information about a person's family, 
@@ -23,23 +23,21 @@ df[df[, "income"] >= 8, "incomeLevel"] = "high"
 file.create("output.txt", showWarnings = TRUE)
 output <-file("output.txt")
 writeLines(c("Average amount of dependents for people don't own their home:", 
-           mean(df[df[, "owner"] == "no", "dependents"]), 
-           "Average amount of age for people with low income:", 
-           mean(df[df[, "incomeLevel"] == "low", "age"]),
-           "Average amount of age for people with average income:",
-           mean(df[df[, "incomeLevel"] == "average", "age"]),
-           "Average amount of age for people with high income:",
-           mean(df[df[, "incomeLevel"] == "high", "age"])),
-           output)
+           mean(df[df[, "owner"] == "no", "dependents"])), output)
+
+sub.df <- aggregate(df["age"], df["incomeLevel"], FUN=mean)
+names(sub.df)[names(sub.df) == "age"] <- "averageAge"
+write("Average age in each income groups:", output)
+write.table(sub.df, file = "output.txt", sep = ";", append=TRUE)
 
 dfSorted <- df[order(df[,"age"]),]
-dfSorted <- dfSorted[dfSorted[,"card"] == "no",]
+dfSorted <- subset(dfSorted, card == "no",)
 write("Top 5 youngest people, whose application was declined: ", 
            file = "output.txt", append=TRUE)
 write.table(dfSorted[1:5,], file = "output.txt", sep = ";", append=TRUE)
 write("Top 5 eldest people, whose application was declined: ", 
       file = "output.txt", append=TRUE)
-write.table(tail(dfSorted, 5), file = "output.txt", sep = ";", append=TRUE, col.names = FALSE)
+write.table(tail(dfSorted, 5), file = "output.txt", sep = ";", append=TRUE)
 
 dfSorted <- df[order(df[,"income"]),]
 write("Average number of major CCs held for people with top 10 income: ", 

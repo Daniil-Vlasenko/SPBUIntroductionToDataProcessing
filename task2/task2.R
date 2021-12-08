a48aa59db84f771c208c84de7f05a0cfc7729aaa
Daniil-Vlasenko <74912458+Daniil-Vlasenko@users.noreply.github.com>
2021-12-08 22:20:58 +0300

    .

--- task2/task2.R
+++ task2/task2.R
@@ -1,71 +1,70 @@
-if (!require("latex2exp")) install.packages("latex2exp")
-if (!require("dplyr")) install.packages("dplyr")
 library("latex2exp")
 library("dplyr")
 
-# 1. Rn has a limit distribution for n -> inf 
-RNorm <- function(x, a, mean = 0, sd = 1) {
+a <- 1/10
+stRenyiCriterionUnif <- function(n, a, right = 0, left = 1) {
+  x <- runif(n, right, left)
   emp.cdf <- ecdf(x)
-  n = length(x)
-  df <- data.frame(emp.cdf = emp.cdf(x), pnorm = pnorm(x, mean, sd))
-  res <- (abs(df$emp.cdf - df$pnorm) / df$pnorm)[df$pnorm > a] %>% max() * sqrt(n)
+  df <- data.frame(emp.cdf = emp.cdf(x), punif = punif(x, right, left)) %>% filter(punif > a)
+  (abs(df$emp.cdf - df$punif) / df$punif) %>% max() * sqrt(n)
 }
-RnNorm <- function(n, a, mean = 0, sd = 1) {
-  x <- sapply(10:n, rnorm, mean, sd)
-  res <- sapply(x, RNorm, a, mean, sd)
-}
-RExp <- function(x, a, r) {
+stRenyiCriterionNorm <- function(n, a, mean = 0, sd = 1) {
+  x <- rnorm(n, mean, sd)
   emp.cdf <- ecdf(x)
-  n = length(x)
-  df <- data.frame(emp.cdf = emp.cdf(x), pexp = pexp(x, r))
-  res <- (abs(df$emp.cdf - df$pexp) / df$pexp)[df$pexp > a] %>% max() * sqrt(n)
+  df <- data.frame(emp.cdf = emp.cdf(x), pnorm = pnorm(x, mean, sd)) %>% filter(pnorm > a)
+  (abs(df$emp.cdf - df$pnorm) / df$pnorm) %>% max() * sqrt(n)
 }
-RnExp <- function(n, a, r) {
-  x <- sapply(10:n, rexp, r)
-  res <- sapply(x, RExp, a, r)
+stRenyiCriterionExp <- function(n, a, y = 1) {
+  x <- rexp(n, y)
+  emp.cdf <- ecdf(x)
+  df <- data.frame(emp.cdf = emp.cdf(x), pexp = pexp(x, y)) %>% filter(pexp > a)
+  (abs(df$emp.cdf - df$pexp) / df$pexp) %>% max() * sqrt(n)
 }
 
+# 1.
 pdf(file="1.pdf")
-par(mfrow=c(2,2))
-hist(RnNorm(100, 1/2), breaks = 10, xlim = c(0, 3), col = "cyan1",  main = "n = 100", xlab = "Rn")
-hist(RnNorm(500, 1/2), breaks = 15, xlim = c(0, 3), col = "cyan1", main = "n = 500", xlab = "Rn")
-hist(RnNorm(1000, 1/2), breaks = 15, xlim = c(0, 3), col = "cyan1", main = "n = 1000", xlab = "Rn")
-hist(RnNorm(5000, 1/2), breaks = 15, xlim = c(0, 3), col = "cyan1", main = "n = 5000", xlab = "Rn")
+plot(density(replicate(500, stRenyiCriterionUnif(10, a))), xlim = c(0, 6), ylim = c(0, 0.5),
+     main = TeX("$R_n$ has a limiting distribution at $n \\rightarrow \\infty$"), col = "black", 
+     xlab = "statistics of the Renyi criterion")
+lines(density(replicate(500, stRenyiCriterionUnif(50, a))), col = "brown")
+lines(density(replicate(500, stRenyiCriterionUnif(100, a))), col = "chartreuse4")
+lines(density(replicate(500, stRenyiCriterionUnif(500, a))), col = "darkgoldenrod3")
+lines(density(replicate(500, stRenyiCriterionUnif(1000, a))), col = "blueviolet")
+legend(4, 0.5, legend=c("n = 10", "n = 50", "n = 100", "n = 500", "n = 1000"),
+       col=c("black", "brown", "chartreuse4", "darkgoldenrod3", "blueviolet"), 
+       lty=1, bg = "whitesmoke")
 dev.off()
-# 2. Asymptotic distribution of Rn is independent of the distribution function F(x).
+# 2.
 pdf(file="2.pdf")
-par(mfrow=c(3,1))
-hist(RnNorm(3000, 1/2), breaks = 15, xlim = c(0, 3), col = "cyan1", main = "N(0, 1)", xlab = "Rn")
-hist(RnNorm(3000, 1/2, 50, 4), breaks = 15, xlim = c(0, 3), col = "cyan1", main = "N(50, 4)", xlab = "Rn")
-hist(RnExp(3000, 1/2, 1), breaks = 15, xlim = c(0, 3), col = "cyan1", main = "EXP(1)", xlab = "Rn")
+plot(density(replicate(500, stRenyiCriterionUnif(1000, a))), xlim = c(0, 6), ylim = c(0, 0.5), 
+     main = TeX("The asymptotic distribution of $R_n$ does not depend on the distribution function $F(x)$"), col = "black", 
+     xlab = "statistics of the Renyi criterion")
+lines(density(replicate(500, stRenyiCriterionExp(1000, a))), col = "brown")
+lines(density(replicate(500, stRenyiCriterionNorm(1000, a))), col = "chartreuse4")
+legend(3.5, 0.5, legend=c("uniform", "exponential", "normal"),
+       col=c("black", "brown", "chartreuse4"), 
+       lty=1, bg = "whitesmoke")
 dev.off()
-# 3. Check statement lim_{n -> inf}{P(sqrt(a / (1 - a))R_{n}^{+} < x)} = 2Ф(x) - 1
-RPlusNorm <- function(n, a) {
-  x <- rnorm(n)
+# 3.
+stRenyiCriterionUnifPlus <- function(n, a, right = 0, left = 1) {
+  x <- runif(n, right, left)
   emp.cdf <- ecdf(x)
-  n = length(x)
-  df <- data.frame(emp.cdf = emp.cdf(x), pnorm = pnorm(x))
-  res <- ((df$emp.cdf - df$pnorm) / df$pnorm)[df$pnorm > a] %>% max() * sqrt(n)
+  df <- data.frame(emp.cdf = emp.cdf(x), punif = punif(x, right, left)) %>% filter(punif > a)
+  ((df$emp.cdf - df$punif) / df$punif) %>% max() * sqrt(n)
 }
-distributionFunction <- function(y, n, m, a) {
-  res <- sapply(rep(n , m), RPlusNorm, a)
-  emp.cdf <- ecdf(res)
-  emp.cdf(y)
-}
+
 pdf(file="3.pdf")
 par(lwd = 2)
-x <- seq(0, 4, length=100)
-y1 <- pnorm(x) * 2 - 1
-y2 <- sapply(x * 1, distributionFunction, 10, 1000, 1/2)
-y3 <- sapply(x * 1, distributionFunction, 50, 1000, 1/2)
-y4 <- sapply(x * 1, distributionFunction, 100, 1000, 1/2)
-y5 <- sapply(x * 1, distributionFunction, 500, 1000, 1/2)
-plot(x,y1, type = "l", main = TeX('$\\lim_{n->inf}(P(\\sqrt{a / (1 - a)}R_{n}^{+} < x)) = 2\\Phi(x) - 1$'),
-     xlab = "x", ylab = "probability")
-lines(x,y2, col = "cyan1")
-lines(x,y3, col = "brown1")
-lines(x,y4, col = "green")
-lines(x,y5, col = "darkgoldenrod1")
-legend(3.05, 0.4, legend=c("n = 10", "n = 50", "n = 100", "n = 500"),
-       col=c("cyan1", "brown1", "green", "darkgoldenrod1"), lty=1, bg = "whitesmoke")
+x <- seq(0, 3.5, length = 300)
+y1 <- ecdf(replicate(300, sqrt(a/(1-a)) * stRenyiCriterionUnifPlus(10, a)))
+y2 <- ecdf(replicate(300, sqrt(a/(1-a)) * stRenyiCriterionUnifPlus(100, a)))
+y3 <- ecdf(replicate(300, sqrt(a/(1-a)) * stRenyiCriterionUnifPlus(1000, a)))
+plot(x, pnorm(x) * 2 - 1, type = "l", xlim = c(0, 3.5), ylab = "y",
+     main = TeX('Demonstration of the equation $\\lim_{n->inf}(P(\\sqrt{a / (1 - a)}R_{n}^{+} < x)) = 2\\Phi(x) - 1$'))
+lines(x, y1(x), col = "brown")
+lines(x, y2(x), col = "chartreuse4")
+lines(x, y3(x), col = "darkgoldenrod3")
+legend(2.5, 0.2, legend=c("n = 10", "n = 100", "n = 1000"),
+       col=c("brown", "chartreuse4", "darkgoldenrod3"), 
+       lty=1, bg = "whitesmoke")
 dev.off()

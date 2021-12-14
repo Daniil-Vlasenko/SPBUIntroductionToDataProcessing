d64de8a0fb4213161ae3aaf4b8a239198ee18c42
Daniil-Vlasenko <74912458+Daniil-Vlasenko@users.noreply.github.com>
2021-12-14 22:37:22 +0300

    .

--- task1/task1.R
+++ task1/task1.R
@@ -1,5 +1,3 @@
-if (!require("lubridate")) install.packages("lubridate")
-if (!require("tidyr")) install.packages("dplyr")
 library("lubridate")
 library("dplyr")
 
@@ -9,25 +7,24 @@ df <- read.table("study_fall2021_intror_creditcard.txt", header=TRUE, sep=";")
 df <- filter(df, selfemp == "yes")
 # Convert amount of months applicant living at his/her current address to days.
 today <- as.Date("2000-01-01")
-df$months <- as.numeric(today - (today %m-% months(df$months)))
-names(df)[names(df) == "months"] <- "days"
+df <- mutate(df, days = as.numeric(today - (today %m-% months(df$months))))
 # Add column with income per family member.
 # Due to the lack of information about a person's family, 
 # we assume that dependents are family members without the applicant.
 df <- mutate(df, incomePerFamilyMember = income / (dependents + 1))
 # Define criterion of income level, and split data according to levels of this criterion.
-incomeLevel <- function(x){
-  if (x < 2.5) "low"
-  else if (x <= 8) "average"
-  else "high"
-}
-df$incomeLevel <- sapply(df$income, FUN =  incomeLevel)
+df$incomeLevel <- case_when(
+  df$income < 2.5 ~ "low",
+  df$income >= 2.5 & df$income <= 8 ~ "average",
+  df$income >8 ~ "high"
+)
 # 2.
 # Print some information about dataset.
 file.create("output.txt", showWarnings = TRUE)
 output <-file("output.txt")
 write("Average amount of dependents for people don't own their home:", file="output.txt")
-write(mean(filter(df, owner == "no")$dependents), file="output.txt", append=TRUE)
+write((filter(df, owner == "no") %>% summarise(meanDependents = mean(dependents)))$meanDependents, 
+      file="output.txt", append=TRUE)
 
 write("Average age in each income groups:", file="output.txt", append=TRUE)
 write.table(df %>% group_by(incomeLevel) %>% summarise(averageAge = mean(age)), 
@@ -36,13 +33,13 @@ write.table(df %>% group_by(incomeLevel) %>% summarise(averageAge = mean(age)),
 write("Top 5 youngest people, whose application was declined: ", 
            file = "output.txt", append=TRUE)
 dftmp <- filter(df, card == "no")
-dftmp <- dftmp[order(dftmp$age),]
+dftmp <- arrange(dftmp, age)
 write.table(dftmp[1:5,], file = "output.txt", sep = ";", append=TRUE)
 write("Top 5 eldest people, whose application was declined: ", file = "output.txt", append=TRUE)
 write.table(tail(dftmp, 5), file = "output.txt", sep = ";", append=TRUE)
 
 write("Average number of major CCs held for people with top 10 income: ", 
       file = "output.txt", append=TRUE)
-write(sort(df$income) %>% tail(10) %>% mean(), 
+write((arrange(dftmp, income) %>% tail(10))$majorcards %>% mean(), 
       file = "output.txt", append=TRUE)
 close(output)
---
+++ task2/task2_2.R
@@ -0,0 +1,60 @@
+library("latex2exp")
+library("dplyr")
+
+a <- 1/10
+distribution <- c("uniform" = punif, "exponential" = pexp, "normal" = pnorm)
+rand <- c("uniform" = runif, "exponential" = rexp, "normal" = rnorm)
+stRenyiCriterion <- function(x, a, distribution, ...) {
+  f <- ecdf(x)
+  df <- data.frame(edf = environment(f)$y, distribution = distribution(environment(f)$x, ...)) %>% filter(edf > a)
+  (abs(df$edf - df$distribution) / df$edf) %>% max() * sqrt(length(x))
+}
+stRenyiCriterionPlus <- function(x, a, distribution, ...) {
+  f <- ecdf(x)
+  df <- data.frame(edf = environment(f)$y, distribution = distribution(environment(f)$x, ...)) %>% filter(edf > a)
+  ((df$edf - df$distribution) / df$edf) %>% max() * sqrt(length(x))
+}
+# 1.
+pdf(file="1.pdf")
+plot(density(replicate(500, stRenyiCriterion(runif(10), a, distribution$uniform))), 
+     xlim = c(0, 6), ylim = c(0, 0.8),
+     main = TeX("$R_n$ has a limiting distribution at $n \\rightarrow \\infty$"), col = "black", 
+     xlab = "statistics of the Renyi criterion")
+lines(density(replicate(500, stRenyiCriterion(rand$uniform(50), a, distribution$uniform))), col = "brown")
+lines(density(replicate(500, stRenyiCriterion(rand$uniform(100), a, distribution$uniform))), col = "chartreuse4")
+lines(density(replicate(500, stRenyiCriterion(rand$uniform(500), a, distribution$uniform))), col = "darkgoldenrod3")
+lines(density(replicate(500, stRenyiCriterion(rand$uniform(1000), a, distribution$uniform))), col = "blueviolet")
+legend(4.5, 0.8, legend=c("n = 10", "n = 50", "n = 100", "n = 500", "n = 1000"),
+       col=c("black", "brown", "chartreuse4", "darkgoldenrod3", "blueviolet"), 
+       lty=1, bg = "whitesmoke")
+dev.off()
+# 2.
+pdf(file="2.pdf")
+plot(density(replicate(500, stRenyiCriterion(rand$uniform(5000), a, distribution$uniform))), 
+     xlim = c(0, 10), ylim = c(0, 0.5), 
+     main = TeX("The asymptotic distribution of $R_n$ does not depend on the distribution function $F(x)$"), col = "black", 
+     xlab = "statistics of the Renyi criterion")
+lines(density(replicate(500, stRenyiCriterion(rand$exponential(5000), a, distribution$exponential))), 
+      col = "brown")
+lines(density(replicate(500, stRenyiCriterion(rand$normal(5000), a, distribution$normal))), 
+      col = "chartreuse4")
+legend(7.5, 0.5, legend=c("uniform", "exponential", "normal"),
+       col=c("black", "brown", "chartreuse4"), 
+       lty=1, bg = "whitesmoke")
+dev.off()
+# 3.
+pdf(file="3.pdf")
+par(lwd = 2)
+x <- seq(0, 3.5, length = 300)
+plot(x, pnorm(x) * 2 - 1, type = "l", xlim = c(0, 3.5), ylab = "y",
+     main = TeX('Demonstration of the equation $\\lim_{n->inf}(P(\\sqrt{a / (1 - a)}R_{n}^{+} < x)) = 2\\Phi(x) - 1$'))
+f <- ecdf(replicate(300, sqrt(a/(1-a)) * stRenyiCriterionPlus(rand$uniform(10), a, distribution$uniform)))
+lines(environment(f)$x, environment(f)$y, col = "brown")
+f <- ecdf(replicate(300, sqrt(a/(1-a)) * stRenyiCriterionPlus(rand$uniform(100), a, distribution$uniform)))
+lines(environment(f)$x, environment(f)$y, col = "chartreuse4")
+f <- ecdf(replicate(300, sqrt(a/(1-a)) * stRenyiCriterionPlus(rand$uniform(1000), a, distribution$uniform)))
+lines(environment(f)$x, environment(f)$y, col = "darkgoldenrod3")
+legend(2.5, 0.2, legend=c("n = 10", "n = 100", "n = 1000"),
+       col=c("brown", "chartreuse4", "darkgoldenrod3"), 
+       lty=1, bg = "whitesmoke")
+dev.off()
